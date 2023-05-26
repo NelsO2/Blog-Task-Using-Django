a3c0829264ac3a138a0a470bac5063c5fca151e2
Nels02 <nelsonchampion19@gmail.com>
2023-05-26 16:53:44 +0100

    updated templates

--- My_Blog/main/models.py
+++ My_Blog/main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django.core.validators import MinValueValidator, MaxValueValidator
+# from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 
 # Create your models here.
@@ -11,7 +11,7 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     published_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now_add=True)
-    content = models.TextField(validators=[MinValueValidator(20)])
+    content = models.TextField()
     # author = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.SlugField()
     post_image = models.ImageField(upload_to='uploads/', default="default_post_image.jpg")
@@ -24,7 +24,7 @@ class Article(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=20)
     subtitle = models.CharField(max_length=20)
-    slug = models.SlugField()
+    # slug = models.SlugField()
 
     def __str__(self):
         return self.title
